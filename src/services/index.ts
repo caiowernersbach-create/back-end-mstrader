4de60519b9d2e86303ea235f2fd5af3048e08e06
[dyad] <git@dyad.sh>
2025-12-16 14:45:41 -0300

    [dyad] Fixed import error by creating missing services directory and files - wrote 5 file(s)

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+// Services index file
+export { tradeService } from './trade.service';
+export { accountService } from './account.service';
+export { assetService } from './asset.service';
+export { strategyService } from './strategy.service';
